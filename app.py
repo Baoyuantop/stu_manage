44ecb9bb5466af6feec35a1b51225f78d8197d4a
Baoyuantop <1045149141@qq.com>
2018-12-30 16:19:49 +0800

    基本完成

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, jsonify, make_response, request
-# from flask.ext.restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 import json
 
 
 app = Flask(__name__)
-# api = Api(app)
+CORS(app)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:byn2316@localhost:3306/flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:******@localhost:3306/flask?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
@@ -25,70 +24,101 @@ class Admin(db.Model):
     number = db.Column(db.String(10), nullable=False)
     password = db.Column(db.String(10), nullable=False)
     def __repr__(self):
-        return '<Role %r>' % self.name
+        return self.number, self.password
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(60), nullable=False)
     number = db.Column(db.String(10), nullable=False)
     age = db.Column(db.String(5), nullable=False)
-    clas = number = db.Column(db.String(10), nullable=False)
-    def __repr__(self):
-        return '%r' % self.username
-
-# users = [
-#     {
-#     'id': 1,
-#     'name': 'baoyuan',
-#     'age': 21
-#     }, {
-#     'id': 2,
-#     'name': 'yenan',
-#     'age': 21
-#     }
-# ]
-
-# @app.shell_context_processor
-# def make_shell_context():
-#     return dict(db=db, User=User, Role=Role)
-
-# @app.route('/users', methods=['GET'])
-# def get_users():
-#     users = User.query.all()
-#     print(users)
-#     resp = Response(users, mimetype="list")
-#     return resp
+    clas = db.Column(db.String(10), nullable=False)
 
 
+# 注册管理员
 @app.route('/users', methods=['POST'])
 def add_users():
-    username = request.form['username']
-    number = request.form['number']
-    password = request.form['password']
+    user = request.get_json()
+    username = user['username']
+    number = user['number']
+    password = user['password']
     id = Admin.query.filter(Admin.number == number).first()
     if id:
-        return 'loged'
+        response = make_response('logged')
+        return response
     else:
         user = Admin(number = number, password = password)
         db.session.add(user)
         db.session.commit()
-        res = make_response('ok')
-        res.headers['Access-Control-Allow-Origin'] = '*'
-        return res
+        response = make_response('ok')
+        return response
 
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return render_template('user.html',name=name)
-# class UserApi(Resource):
-#     def get(self, number):
-#         pass
-#
-#     def put(self, number):
-#         pass
-#
-#     def delete(self, id):
-#         pass
-#
-# api.add_resource(UserApi, '/users/<int: number>', endpoint = 'user')
+# 管理员登陆
+@app.route('/login', methods=['POST'])
+def user_login():
+    user = request.get_json()
+    number = user['number']
+    password = user['password']
+    passwd = db.session.query(Admin.password).filter_by(number=number).first()
+    print(passwd[0])
+    print(password)
+    if password == passwd[0]:
+        response = make_response('true')
+        response.set_cookie(number)
+        return response
+    else:
+        response = make_response('false')
+        return response
+# 增加学生
+@app.route('/add', methods=['POST'])
+def user_add():
+    user = request.get_json()
+    username = user['name']
+    number = user['number']
+    age = user['age']
+    clas = user['clas']
+    id = User.query.filter(User.number == number).first()
+    if id:
+        response = make_response('had')
+        return response
+    else:
+        user = User(username=username, number=number, age=age, clas=clas)
+        db.session.add(user)
+        db.session.commit()
+        response = make_response('ok')
+        return response
+# 读取学生列表
+@app.route('/user', methods=['GET'])
+def get_user():
+    users = User.query.all()
+    student = []
+    for user in users:
+        json_dist = {
+            "name": user.username,
+            "number": user.number,
+            "age": user.age,
+            "clas": user.clas
+        }
+        student.append(json_dist)
+    return jsonify(student)
+# 编辑学生信息
+@app.route('/user/edit', methods=['POST'])
+def edit_user():
+    stu = request.get_json()
+    stu_old = User.query.filter(User.number == stu['number']).first()
+    stu_old.username = stu['name']
+    stu_old.number = stu['number']
+    stu_old.age = stu['age']
+    stu_old.clas = stu['clas']
+    db.session.commit()
+    response = make_response('ok')
+    return response
+# 删除学生
+@app.route('/user/del', methods=['POST'])
+def del_user():
+    stu = request.get_json()
+    student = User.query.filter(User.number == stu['number']).first()
+    db.session.delete(student)
+    db.session.commit()
+    response = make_response('ok')
+    return response
